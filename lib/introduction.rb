0d344bd712b5a440ed3041c5f3d8f4f76417ae90
Ines Angelica Thompson <i.thompson89@outlook.com>
2019-07-10 04:10:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,7 @@ end
 
 #introduction_with_language_optional
 def introduction_with_language_optional(name)
-puts "Hi, my name is #{name} and I am learning to program in #{language}."
+puts "Hi, my name is #{name} and I am learning to program in Ruby."
 end
 
 
